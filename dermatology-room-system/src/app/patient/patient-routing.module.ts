577771664a70d0765a93a78dcba2f9daef62c75f
tanit2503 <congtan2503@gmail.com>
2022-09-20 13:55:21 +0700

    update UI

--- dermatology-room-system/src/app/patient/patient-routing.module.ts
+++ dermatology-room-system/src/app/patient/patient-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PatientListComponent} from './patient-list/patient-list.component';
+import {PatientCreateComponent} from './patient-create/patient-create.component';
+import {PatientUpdateComponent} from './patient-update/patient-update.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list', component: PatientListComponent},
+  {path: 'create', component: PatientCreateComponent},
+  {path: 'update', component: PatientUpdateComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
