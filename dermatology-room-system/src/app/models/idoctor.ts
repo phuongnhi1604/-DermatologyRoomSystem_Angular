7f3e1c4ac26993e4337489c9d3fca64cc8042bb5
tanit2503 <congtan2503@gmail.com>
2022-10-08 22:59:17 +0700

    Update

--- dermatology-room-system/src/app/models/idoctor.ts
+++ dermatology-room-system/src/app/models/idoctor.ts
@@ -1,10 +1,10 @@
 export interface IDoctor {
-    // private int doctor_id;
-    // private String doctor_name;
-    // private String doctor_birthday;
-    // private boolean doctor_gender;
-    // private String doctor_address;
-    // private String doctor_phone;
-    // private String doctor_id_card;
-    // private String doctor_email;
+    doctor_id?: number;
+    doctor_name?: string;
+    doctor_birthday?: string;
+    doctor_gender?: string;
+    doctor_address?: string;
+    doctor_phone?: string;
+    doctor_id_card?: string;
+    doctor_email?: string;
 }
